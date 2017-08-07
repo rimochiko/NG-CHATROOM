9b30ede8867bf4cb5e1dbbe94fd5a6d075142453
mochiko <virmy@qq.com>
2017-08-07 09:24:40 +0800

    NGchat

---
+++ NGchat/NGchat/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace NGchat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_showMsg = new System.Windows.Forms.ListBox();
+            this.tb_inputMsg = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.lb_users = new System.Windows.Forms.ListBox();
+            this.lb_userTitle = new System.Windows.Forms.Label();
+            this.lb_nameTitle = new System.Windows.Forms.Label();
+            this.tb_loginName = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_loginOut = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb_showMsg
+            // 
+            this.lb_showMsg.BackColor = System.Drawing.Color.White;
+            this.lb_showMsg.Font = new System.Drawing.Font("Microsoft YaHei", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb_showMsg.FormattingEnabled = true;
+            this.lb_showMsg.HorizontalScrollbar = true;
+            this.lb_showMsg.ItemHeight = 20;
+            this.lb_showMsg.Location = new System.Drawing.Point(19, 54);
+            this.lb_showMsg.Name = "lb_showMsg";
+            this.lb_showMsg.Size = new System.Drawing.Size(420, 224);
+            this.lb_showMsg.TabIndex = 0;
+            // 
+            // tb_inputMsg
+            // 
+            this.tb_inputMsg.BackColor = System.Drawing.Color.White;
+            this.tb_inputMsg.Font = new System.Drawing.Font("Microsoft YaHei", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tb_inputMsg.Location = new System.Drawing.Point(20, 309);
+            this.tb_inputMsg.Multiline = true;
+            this.tb_inputMsg.Name = "tb_inputMsg";
+            this.tb_inputMsg.Size = new System.Drawing.Size(419, 74);
+            this.tb_inputMsg.TabIndex = 1;
+            this.tb_inputMsg.TextChanged += new System.EventHandler(this.tb_inputMsg_TextChanged);
+            // 
+            // btn_send
+            // 
+            this.btn_send.Enabled = false;
+            this.btn_send.Location = new System.Drawing.Point(364, 402);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 2;
+            this.btn_send.Text = "发送";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // lb_users
+            // 
+            this.lb_users.BackColor = System.Drawing.Color.White;
+            this.lb_users.Font = new System.Drawing.Font("Microsoft YaHei", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb_users.FormattingEnabled = true;
+            this.lb_users.HorizontalScrollbar = true;
+            this.lb_users.ItemHeight = 20;
+            this.lb_users.Location = new System.Drawing.Point(459, 55);
+            this.lb_users.Name = "lb_users";
+            this.lb_users.Size = new System.Drawing.Size(155, 364);
+            this.lb_users.TabIndex = 5;
+            this.lb_users.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lb_users_MouseClick);
+            // 
+            // lb_userTitle
+            // 
+            this.lb_userTitle.AutoSize = true;
+            this.lb_userTitle.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb_userTitle.Location = new System.Drawing.Point(456, 20);
+            this.lb_userTitle.Name = "lb_userTitle";
+            this.lb_userTitle.Size = new System.Drawing.Size(80, 17);
+            this.lb_userTitle.TabIndex = 6;
+            this.lb_userTitle.Text = "其他在线用户";
+            // 
+            // lb_nameTitle
+            // 
+            this.lb_nameTitle.AutoSize = true;
+            this.lb_nameTitle.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb_nameTitle.Location = new System.Drawing.Point(17, 20);
+            this.lb_nameTitle.Name = "lb_nameTitle";
+            this.lb_nameTitle.Size = new System.Drawing.Size(56, 17);
+            this.lb_nameTitle.TabIndex = 7;
+            this.lb_nameTitle.Text = "用户名：";
+            // 
+            // tb_loginName
+            // 
+            this.tb_loginName.BackColor = System.Drawing.Color.White;
+            this.tb_loginName.Location = new System.Drawing.Point(79, 19);
+            this.tb_loginName.Name = "tb_loginName";
+            this.tb_loginName.Size = new System.Drawing.Size(164, 21);
+            this.tb_loginName.TabIndex = 8;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(270, 18);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 9;
+            this.btn_login.Text = "登录";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // btn_loginOut
+            // 
+            this.btn_loginOut.Enabled = false;
+            this.btn_loginOut.Location = new System.Drawing.Point(364, 18);
+            this.btn_loginOut.Name = "btn_loginOut";
+            this.btn_loginOut.Size = new System.Drawing.Size(75, 23);
+            this.btn_loginOut.TabIndex = 10;
+            this.btn_loginOut.Text = "退出";
+            this.btn_loginOut.UseVisualStyleBackColor = true;
+            this.btn_loginOut.Click += new System.EventHandler(this.btn_loginOut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(636, 442);
+            this.Controls.Add(this.btn_loginOut);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.tb_loginName);
+            this.Controls.Add(this.lb_nameTitle);
+            this.Controls.Add(this.lb_userTitle);
+            this.Controls.Add(this.lb_users);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.tb_inputMsg);
+            this.Controls.Add(this.lb_showMsg);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "年糕聊天室";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lb_showMsg;
+        private System.Windows.Forms.TextBox tb_inputMsg;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.ListBox lb_users;
+        private System.Windows.Forms.Label lb_userTitle;
+        private System.Windows.Forms.Label lb_nameTitle;
+        private System.Windows.Forms.TextBox tb_loginName;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_loginOut;
+    }
+}
+
---
+++ NGchat/NGchat/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace NGchat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            setRestStatus();
+        }
+
+
+        //判断聊天窗口是否有改动信息
+        bool isModify = false;
+
+        Socket socketClient = null;
+        Thread threadClient = null;
+
+        //用于安全结束线程的标记
+        bool isntStop = true;
+
+        //点击登录按钮
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            //获取用户名
+            string userName = tb_loginName.Text.Trim();
+            isntStop = true;
+
+            //判断用户名是否输入正确
+
+            if (string.IsNullOrEmpty(userName))
+            {
+                MessageBox.Show("请填写用户名");
+                return;
+            }
+
+            if (userName.Contains('*') || userName.Contains("管理员"))
+            {
+                MessageBox.Show("请不要在用户名填写不符合规定字符词语");
+                return;
+            }
+
+            try
+            {
+                //建立客户端的Socket
+                socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                //建立服务器的IP连接地址
+                IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
+                EndPoint point = new IPEndPoint(ipaddress, 7788);
+
+                //客户端连接服务器
+                socketClient.Connect(point);
+
+                //开启监听来自服务器的信息的线程
+                threadClient = new Thread(RecMsg);
+                threadClient.IsBackground = true;
+                threadClient.Start();
+
+                setWorkStatus();
+
+                //给服务器发送用户名信息
+                byte[] arrClientSendMsg = Encoding.UTF8.GetBytes("1*" + userName);
+                socketClient.Send(arrClientSendMsg);
+
+                lb_showMsg.Items.Add("【年糕聊天室】欢迎用户" + userName + "登陆聊天室");
+            }
+            catch
+            {
+                MessageBox.Show("与服务器连接失败");
+            }
+
+        }
+
+
+        //监听来自服务器的消息
+        private void RecMsg()
+        {
+            try
+            {
+                while (isntStop)
+                {
+                    //创建一个字节对象来储存服务器的信息
+                    byte[] arrRecMsg = new byte[1024 * 1024];
+                    int length = socketClient.Receive(arrRecMsg);
+                    string strMsg = Encoding.UTF8.GetString(arrRecMsg, 0, length);
+
+                    string typeMsg = strMsg.Split('*')[0];
+                    string info = strMsg.Substring(2);
+
+                    switch (typeMsg)
+                    {
+                        //其他用户上线的通知
+                        case "1":
+                            lb_users.Items.Add(info);
+                            lb_showMsg.Items.Add("用户" + info.Split('|')[0] + "上线啦！");
+                            lb_showMsg.SelectedIndex = lb_showMsg.Items.Count - 1;
+                            break;
+
+                        //获取用户在线列表
+                        case "2":
+                            string[] list = info.Split('&');
+                            for (int i = 0; i < list.Length - 1; i++)
+                            {
+                                lb_users.Items.Add(list[i]);
+                            }
+                            break;
+
+                        //其他用户退出的通知
+                        case "3":
+                            lb_users.Items.Remove(info);
+                            lb_showMsg.Items.Add(info.Split('|')[0] + "退出了登录");
+                            lb_showMsg.SelectedIndex = lb_showMsg.Items.Count - 1;
+                            break;
+
+                        //收到群聊的消息
+                        case "4":
+                            lb_showMsg.Items.Add(info);
+                            lb_showMsg.SelectedIndex = lb_showMsg.Items.Count - 1;
+                            break;
+
+                        //收到单聊的消息
+                        case "5":
+                            info = strMsg.Split('*')[1];
+                            lb_showMsg.Items.Add(info);
+                            lb_showMsg.SelectedIndex = lb_showMsg.Items.Count - 1;
+                            break;
+
+                        //收到管理员的广播
+                        case "6":
+                            lb_showMsg.Items.Add("管理员:" + info);
+                            lb_showMsg.SelectedIndex = lb_showMsg.Items.Count - 1;
+                            break;
+
+                        //收到服务器被关闭的通知
+                        case "7":
+                            lb_showMsg.Items.Add("聊天服务器已被关闭，谢谢您的使用。");
+                            lb_showMsg.SelectedIndex = lb_showMsg.Items.Count - 1;
+                            safeClose();
+                            isntStop = false;
+                            break;
+                        //收到服务器确认用户退出的通知
+                        case "8":
+                            safeClose();
+                            isntStop = false;
+                            break;
+                    }
+                }
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+
+        }
+
+        //点击发送信息按钮
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lb_users.SelectedIndex == -1)
+                {
+                    //如果是向所有人发送消息
+                    string sendMsg = "4*" + tb_loginName.Text + "*" + tb_inputMsg.Text.Trim();
+                    byte[] arrClientSendMsg = Encoding.UTF8.GetBytes(sendMsg);
+                    socketClient.Send(arrClientSendMsg);
+                }
+                else
+                {
+                    //如果是指定向某个用户发送消息
+                    string user = lb_users.SelectedItem.ToString();
+                    string sendMsg = "5*" + tb_loginName.Text + "*" + user + "*" + tb_inputMsg.Text.Trim();
+                    byte[] arrClientSendMsg = Encoding.UTF8.GetBytes(sendMsg);
+                    socketClient.Send(arrClientSendMsg);
+                    lb_showMsg.Items.Add("你对 " + user.Split('|')[0] + " 说：" + tb_inputMsg.Text.Trim());
+                    lb_showMsg.SelectedIndex = lb_showMsg.Items.Count - 1;
+                }
+                tb_inputMsg.Text = "";
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+        }
+
+        //安全关闭socket
+        public void safeClose()
+        {
+            try
+            {
+                if (socketClient == null)
+                    return;
+
+                if (!socketClient.Connected)
+                    return;
+
+                socketClient.Shutdown(SocketShutdown.Both);
+                socketClient.Close();
+            }
+            catch
+            {
+            }
+
+            lb_showMsg.Items.Add("您已退出了聊天室");
+            lb_showMsg.Items.Add("---------以上为历史聊天记录-----------");
+            lb_users.Items.Clear();
+            setRestStatus();
+
+        }
+
+        //退出登陆
+        private void btn_loginOut_Click(object sender, EventArgs e)
+        {
+            byte[] arrClientSendMsg = Encoding.UTF8.GetBytes("3*");
+            socketClient.Send(arrClientSendMsg);
+        }
+
+        //关闭窗体
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (isModify && socketClient != null)
+                {
+                    if (MessageBox.Show("输入区已经改动但未发送，您还要继续关闭吗", "确认关闭", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+                    {
+                        byte[] arrClientSendMsg = Encoding.UTF8.GetBytes("3*");
+                        socketClient.Send(arrClientSendMsg);
+                        return;
+                    }
+                    e.Cancel = true;
+                }
+
+                if (socketClient != null && socketClient.Connected)
+                {
+                    byte[] arrClientSendMsg = Encoding.UTF8.GetBytes("3*");
+                    socketClient.Send(arrClientSendMsg);
+                }
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+
+
+        }
+
+        //服务未开启时控件的可用性
+        public void setRestStatus()
+        {
+            tb_loginName.Enabled = true;
+            tb_inputMsg.Enabled = false;
+            btn_send.Enabled = false;
+            btn_login.Enabled = true;
+            btn_loginOut.Enabled = false;
+
+        }
+
+        //服务开启时控件的可用性
+        public void setWorkStatus()
+        {
+            tb_loginName.Enabled = false;
+            btn_loginOut.Enabled = true;
+            btn_send.Enabled = true;
+            btn_login.Enabled = false;
+            tb_inputMsg.Enabled = true;
+        }
+
+        //如果聊天窗口存在未发送的消息
+        private void tb_inputMsg_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tb_inputMsg.Text.Trim()))
+            {
+                isModify = true;
+            }
+            else
+            {
+                isModify = false;
+            }
+        }
+
+        //当点击用户列表空白处，取消选择单聊对象
+        private void lb_users_MouseClick(object sender, MouseEventArgs e)
+        {
+            var index = lb_users.IndexFromPoint(e.X, e.Y);
+            lb_users.SelectedIndex = index;
+        }
+    }
+}
